63dc1bc560e2a791874d998f13a4aa9ece7318b1
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-11 14:15:26 +0800

    [jianglin] feat: finished parctice 1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = ({ data }) => {
+  return data.map(ele => {
+    const [name, age, gender, birthday] = ele;
+    return {
+      name,
+      age,
+      gender,
+      birthday
+    }
+  })
+  
+}
 export { parseData };
