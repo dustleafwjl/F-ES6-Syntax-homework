caf34a717d129e255718800c96bd8b21e722e42c
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-11 14:18:03 +0800

    [jianglin] feat: finfished parcirce 2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,7 @@
-const inject
+const inject = (items, sections) => {
+  sections.reverse().forEach(item => {
+    items.splice(item.index, 0, item.content)
+  })
+  return items;
+}
 export { inject };
