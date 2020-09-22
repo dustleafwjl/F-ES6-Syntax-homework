b6ffff6471da7f7438bca6abc67d3ccf20e0e4c8
kevindongzg <dongkevin94@gmail.com>
2020-09-22 17:14:13 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,5 +1,7 @@
 const parseData = ({ data }) => {
+  // TODO 箭头函数直接return可以简化写法
   return data.map(ele => {
+    // TODO 这四个key是后端返回的，不要写死
     const [name, age, gender, birthday] = ele;
     return {
       name,
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,6 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+  // TODO 这里应该做sort，而不是reverse
   sections.reverse().forEach(item => {
     items.splice(item.index, 0, item.content)
   })
